3953505625c02f991906104d79d48968a0e797e0
mosspanic <daw_dash@mail.ru>
2021-10-02 16:24:10 +0500

    begin

---
+++ app/src/main/java/com/on/fsp/BattonActivity.kt
@@ -0,0 +1,11 @@
+package com.on.fsp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BattonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batton)
+    }
+}
---
+++ app/src/main/java/com/on/fsp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.on.fsp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val butn = findViewById<ImageButton>(R.id.mainActivityButton)
+        butn!!.setOnClickListener(View.OnClickListener {
+
+            val data = Intent(this ,BattonActivity::class.java)
+
+            startActivity(data)
+        })
+    }
+}
